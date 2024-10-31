888c1bb59db2d47ddb05466e15b7de53351e21f3
Simone Canty <shoe200girl@gmail.com>
2024-10-30 22:45:20 -0400

    Update changelog for version 0.2.0 with new exercises

--- my_script.py
+++ my_script.py
@@ -1,61 +1,61 @@
-# my_list = [0, 1, 2, 3, 4, 5]
-# print(my_list[1:4])
-# print(my_list[:3])
-# print(my_list[2:])
-# print(my_list[0:5:2])
-# print(my_list[-3:])
-# my_favorite_movies =['And Justice for All', 'Annie Hall', 'The Equalizer', 'Outlaw of Josey Wales', 'Rocky' ]
-# my_favorite_movies.append('Star Wars')
-# print(my_favorite_movies)
-# my_favorite_movies.remove('Annie Hall')
-# print(my_favorite_movies)
-# my_list = (0, 1, 2, 3, 4, 5)
-# print(my_list[4:6])
-# colors = ['red', 'blue', 'green']
-# colors.insert(2, 'yellow')
-# print(colors)
-# colors.insert(5, 'purple')
-# print(colors)
-# dimensions = ( 30, 40, 63)
-# print(dimensions[1])
-# numbers = (0, 1, 2, 3, 4, 5, 6, 7, 8)
-# print(numbers[2:6])
-# fruits = ('apple', 'banana')
-# vegetables = ('carrot', 'lettuce')
-# groceries = (fruits + vegetables)
-# print(groceries)
+my_list = [0, 1, 2, 3, 4, 5]
+print(my_list[1:4])
+print(my_list[:3])
+print(my_list[2:])
+print(my_list[0:5:2])
+print(my_list[-3:])
+my_favorite_movies =['And Justice for All', 'Annie Hall', 'The Equalizer', 'Outlaw of Josey Wales', 'Rocky' ]
+my_favorite_movies.append('Star Wars')
+print(my_favorite_movies)
+my_favorite_movies.remove('Annie Hall')
+print(my_favorite_movies)
+my_list = (0, 1, 2, 3, 4, 5)
+print(my_list[4:6])
+colors = ['red', 'blue', 'green']
+colors.insert(2, 'yellow')
+print(colors)
+colors.insert(5, 'purple')
+print(colors)
+dimensions = ( 30, 40, 63)
+print(dimensions[1])
+numbers = (0, 1, 2, 3, 4, 5, 6, 7, 8)
+print(numbers[2:6])
+fruits = ('apple', 'banana')
+vegetables = ('carrot', 'lettuce')
+groceries = (fruits + vegetables)
+print(groceries)
 
-# book = {'title': '1984', 'author': 'George Orwell', 'year': 1949}
-# print(book['author'])
+book = {'title': '1984', 'author': 'George Orwell', 'year': 1949}
+print(book['author'])
 
-# profile = {'name': 'Mary', 'age': 35, 'city': 'Philadelphia'}
-# profile['city'] = 'London'
-# print(profile)
-
-
-# student = {'name': 'Emma', 'grade': 'A', 'subject': 'Math'}
-# student.pop('subject')
-# print(student)
+profile = {'name': 'Mary', 'age': 35, 'city': 'Philadelphia'}
+profile['city'] = 'London'
+print(profile)
 
 
-# print(student.keys())
-# print(student.values())
+student = {'name': 'Emma', 'grade': 'A', 'subject': 'Math'}
+student.pop('subject')
+print(student)
 
-# fruits = {'apple', 'banana', 'cherry'}
-# fruits.add('orange')
-# print(fruits)
 
-# fruits.remove('banana')
-# print(fruits)
+print(student.keys())
+print(student.values())
 
-# set_a = {1, 2, 3, 4}
-# set_b = {3, 4, 5, 6}
-# print(set_a.union(set_b))
-# print(set_a.intersection(set_b))
+fruits = {'apple', 'banana', 'cherry'}
+fruits.add('orange')
+print(fruits)
 
-# set_x = {'cat', 'dog', 'fish'}
-# set_y = {'dog', 'bird'}
-# print(set_x.difference(set_y))
+fruits.remove('banana')
+print(fruits)
+
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+print(set_a.union(set_b))
+print(set_a.intersection(set_b))
+
+set_x = {'cat', 'dog', 'fish'}
+set_y = {'dog', 'bird'}
+print(set_x.difference(set_y))
 
 
 
